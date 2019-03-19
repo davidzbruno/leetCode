a5792decd71cdfa5825f26cfaf000c69df29c12e
David Bruno <davidbruno2000@gmail.com>
2019-03-18 20:57:47 -0400

    correct Character class method used

--- problems900-999/p917.java
+++ problems900-999/p917.java
@@ -1,9 +1,19 @@
+/*
+Name: David Bruno
+Start Date: 03/18/2019
+File: p771.java
+
+Solve Date: 03/18/2019
+Runtime: ----
+Memory: ----
+
+*/
 class Solution {
     public String reverseOnlyLetters(String S) {
         String result = "";
         
         for( int i = 0; i < S.length(); ++i){
-            if(Character.isAlpha(S.charAt(i))){
+            if(Character.isLetter(S.charAt(i))){
                 result += S.charAt(S.length() - 1 - i);
             }
             else
