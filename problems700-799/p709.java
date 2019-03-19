a9b273dd5eba284c7092ffe21d8c06968e61740d
David Bruno <davidbruno2000@gmail.com>
2019-03-18 20:24:38 -0400

    initial commit

---
+++ problems700-799/p709.java
@@ -0,0 +1,22 @@
+/*
+Name: David Bruno
+Start Date: 03/18/2019
+File: p771.java
+
+Solve Date: 03/18/2019
+Runtime: 0 ms
+Memory: 36.5 MB
+
+*/
+
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+
+        String res = "";
+        for(char c : str.toCharArray()){
+            res += Character.toLowerCase(c);
+        }
+        return res;
+    }
+}
