7d8e551aab30b52d64e2aa16552e6dcc98d0d300
David Bruno <davidbruno2000@gmail.com>
2019-03-18 21:04:17 -0400

    solved: java (4ms)

--- problems900-999/p917.java
+++ problems900-999/p917.java
@@ -4,17 +4,25 @@ Start Date: 03/18/2019
 File: p771.java
 
 Solve Date: 03/18/2019
-Runtime: ----
-Memory: ----
+Runtime: 4ms
+Memory: 37.2 MB
 
 */
 class Solution {
     public String reverseOnlyLetters(String S) {
         String result = "";
-        
-        for( int i = 0; i < S.length(); ++i){
+        String reversedLetters = "";
+
+        //Add the reversed letters to a string;
+        for(int i = S.length()-1; i >= 0; --i){
+            if(Character.isLetter(S.charAt(i)))
+                reversedLetters += S.charAt(i);
+        }
+
+        //Reconstruct the string but with the reveresed letters
+        for( int i = 0, j = 0; i < S.length(); ++i){
             if(Character.isLetter(S.charAt(i))){
-                result += S.charAt(S.length() - 1 - i);
+                result += reversedLetters.charAt(j++);
             }
             else
                 result += S.charAt(i);
