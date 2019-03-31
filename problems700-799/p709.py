c350625a9347bcbb9a369a7a493336309b58f6d6
David Bruno <davidbruno2000@gmail.com>
2019-03-30 21:38:10 -0400

    solved: python (36ms)

---
+++ problems700-799/p709.py
@@ -0,0 +1,11 @@
+# Name: David Bruno
+# Start Date: 03/30/2019
+# File: p709.java
+#
+# Solve Date: 03/30/2019
+# Runtime: 36 ms
+# Memory: 13.2 MB
+
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        return str.lower()
--- problems700-799/p771.py
+++ problems700-799/p771.py
@@ -3,8 +3,8 @@
 # File: p771.java
 #
 # Solve Date: 03/30/2019
-# Runtime: 7 ms
-# Memory: 38 MB
+# Runtime: 36 ms
+# Memory: 13.1 MB
 
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
