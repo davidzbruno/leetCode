9a22d0972f0d2babff52318e1f1689bff0f1f4d7
David Bruno <davidbruno2000@gmail.com>
2019-03-31 23:56:32 -0400

    sovled: java (0ms)

---
+++ problems200-299/p292.java
@@ -0,0 +1,14 @@
+/*
+// Name: David Bruno
+// Start Date: 03/31/2019
+// File: p292.java
+
+// Solve Date: 03/31/2019
+// Runtime: 0 ms
+// Memory: 31.8 MB
+*/
+class Solution {
+    public boolean canWinNim(int n) {
+        return (n % 4 != 0);
+    }
+}
